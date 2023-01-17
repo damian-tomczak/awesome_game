b9eacee9adaea68480d6919a24ea3be2b3f6f6e4
Damian Tomczak <contact@damian-tomczak.pl>
2023-01-17 16:32:22 +0100

    commit

--- admin/menu.php
+++ admin/menu.php
@@ -1,9 +1,11 @@
 <tr>
     <td id="menu">
-        <a href="./?action=welcome.php">Welcome page</a><br>
-        <a href="./?action=dummy.php">Dummy staff</a><br>
-        <a href="./?action=newsletter/manage_news.php">Newsletter</a><br>
-        <a href="./?action=category/manage_categories.php">Categories</a><br>
-        <!-- <a href="./?action=files/manage_files.php">Files</a><br> -->
-        <a href="./?action=products/manage_products.php">Products</a><br>
+        <div>
+            <a href="./?action=welcome.php">Welcome page</a><br>
+            <a href="./?action=dummy.php">Dummy staff</a><br>
+            <a href="./?action=newsletter/manage_news.php">Newsletter</a><br>
+            <a href="./?action=category/manage_categories.php">Categories</a><br>
+            <!-- <a href="./?action=files/manage_files.php">Files</a><br> -->
+            <a href="./?action=products/manage_products.php">Products</a><br>
+        </div>
     </td>
